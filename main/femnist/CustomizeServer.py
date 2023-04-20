d1631296e9e0d95e78c7de2e9e2258331de49455
StreamAzure <1905105036@qq.com>
2023-04-20 12:28:57 +0000

    femnist

---
+++ main/femnist/CustomizeServer.py
@@ -0,0 +1,114 @@
+from easyfl.server import BaseServer
+from easyfl.server.base import MODEL
+from easyfl.protocol import codec
+import numpy as np
+import logging
+
+
+"""
+Def-KT论文复现
+1. 论文要求每轮选出2Q个客户端，且2Q<<K，K为总客户端数。所以这里暂定Q=5，K=100
+2. 每一轮开始时，A组客户端自行训练。训练完毕后，id为 j 的客户端将模型A传递给id为 (j+Q)%K 的B组客户端
+3. B组客户端将模型A与自己的本地模型B进行相互学习（交叉熵+KL散度），最后将更新的模型A保存作为自己的本地模型
+4. 
+"""
+logger = logging.getLogger(__name__)
+
+K = 10 # 客户端总数
+Q = 2 # 一组客户端数，每轮选两组
+# 注：“每轮随机选择Q个”已由默认的Selection方法实现，
+# 参数由config.yaml中的clients_per_round及random_selection决定
+
+"""
+Server 的默认 train 方法执行顺序：
+1. self.selection()，选出指定的Q个客户端加入self.selected_clients
+2. self.grouping_for_distributed()，用于GPU分组，只用1个GPU就不用管，self.grouped_clients = self.selected_clients
+3. self.compression()，压缩模型，啥也没干
+4. self.distribution_to_train()
+    将模型分发给客户端，并调用客户端的run_train方法让客户端开始训练
+    并自动收集训练完毕的模型，记在uploaded_models和uploaded_weights中
+5. self.aggregation()，
+    将客户端上传的模型聚合，默认聚合方法是FedAvg
+    只做权重分配，实际聚合计算在self.aggregate()中
+"""
+
+class CustomizedServer(BaseServer):
+    """
+    server实际只需要做selection和distribution_to_train，不需要收集模型和聚合计算
+    """
+
+    def __init__(self, conf, **kwargs):
+        super(CustomizedServer, self).__init__(conf, **kwargs)
+        self.A_clients = [] # A组客户端
+        self.B_clients = [] # B组客户端
+
+    def selection(self, clients, clients_per_round):
+        """
+        似乎没办法在distribution_to_train中通过指定ID的形式把模型分发给指定的客户端
+        所以只能在这里把A组和B组一起选出
+        """
+        if clients_per_round > len(clients):
+            logger.warning("Available clients for selection are smaller than required clients for each round")
+
+        clients_per_round = min(clients_per_round, len(clients))
+        # 这里写死了一定是随机选择（默认版本是有选项的）
+        # 选出Q个客户端加入self.A_clients
+
+        # 随机选出Q个客户端加入self.A_clients
+        np.random.seed(self._current_round)
+        self.A_clients = np.random.choice(clients, clients_per_round, replace=False)
+        self.A_clients = self.A_clients.tolist()
+
+        # 再从余下的客户端中选出Q个客户端加入self.B_clients
+        # 论文所说的B组由下标计算确定，感觉没法完全保证A组和B组的客户端不重复
+        # 所以这里换了种方法，直接从剩下的客户端中随机选出Q个
+        self.B_clients = np.random.choice(list(set(clients) - set(self.A_clients)), 
+                                          clients_per_round, replace=False)
+        self.B_clients = self.B_clients.tolist()
+
+    
+    def distribution_to_train_locally(self):
+        """
+        1. 让A组客户端进行本地训练
+        2. 把训练好的A组客户端模型分发给B组客户端进行DML
+        """
+        A_models = []
+        for client in self.A_clients: # 遍历已选出的Q个A组客户端中的每一个
+            self.conf.client.task_id = self.conf.task_id
+            self.conf.client.round_id = self._current_round
+
+            print("client: {}".format(client.cid))
+
+            uploaded_request = client.run_train(self._compressed_model, self.conf.client, train_local_only=True)
+            # A组客户端训练自己的本地模型，分发的self.model实际上不会被用到（应该，吧？）
+            model = self.decompression(codec.unmarshal(uploaded_request.content.data))
+            # 把A组客户端训练好的模型保存为model
+            A_models.append(model)
+
+        for client in self.B_clients: # 遍历已选出的Q个B组客户端中的每一个
+            # 将A组model分发给对应的B组客户端进行DML训练
+            model = A_models[self.B_clients.index(client)]
+            client.run_train(model, self.conf.client, train_local_only=False)
+            
+        clients = self.A_clients + self.B_clients
+        loss = []
+        acc = []
+        for client in self._clients:
+            test_loss, test_acc = client.test_local_model(self.conf, "cuda")
+
+            loss.append(test_loss)
+            acc.append(test_acc)
+        
+        print("----------- acc -----------")
+        print(acc)
+        print("--------- test avarage ---------")
+
+        print("--- All clients' test loss: {:.2f}".format(sum(loss)/len(loss)))
+        
+        print("--- All clients' test acc: {:.2f}%".format(sum(acc)/len(acc)))
+    
+    def aggregation(self):
+        pass # 不对收集上来的客户端模型做任何聚合
+
+    def test(self):
+        pass
---
+++ main/femnist/CustomizedClient.py
@@ -0,0 +1,168 @@
+import time
+import copy
+from torch import nn
+
+from easyfl.client.base import BaseClient
+import  torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+from utils import accuracy
+# from local_cnn import CNNModel
+from simple_cnn import Model
+from easyfl.tracking import metric
+from easyfl.tracking.evaluation import model_size
+
+DML_lr = 0.015 # DML训练的学习率
+
+local_batch_size = 64 # B_1
+DML_batch_size = 16 # B_2
+# 论文里区分了两个阶段的batch size为B_1和B_2
+
+local_epoch = 1 # M
+DML_epoch = 25 # E
+
+alpha = 1 # KL散度的权重
+beta = 1 # 交叉熵的权重
+
+class CustomizedClient(BaseClient):
+    def __init__(self, cid, conf, train_data, test_data, device, **kwargs):
+        super(CustomizedClient, self).__init__(cid, conf, train_data, test_data, device, **kwargs)
+        # Initialize a classifier for each client.
+        # self.local_model= CNNModel()
+        self.local_model = Model()
+        print("local_batch_size: {}, DML_batch_size: {}, local_epoch: {}, DML_epoch: {}, alpha: {}, beta: {}, DML_lr: {}".format(local_batch_size, DML_batch_size, local_epoch, DML_epoch, alpha, beta, DML_lr))
+
+    def train_local(self, conf, device):
+        self.local_model.train()
+        self.local_model.to(device)
+        local_optimizer = torch.optim.SGD(self.local_model.parameters(),
+                                        lr=conf.optimizer.lr,
+                                        momentum=conf.optimizer.momentum,
+                                        weight_decay=conf.optimizer.weight_decay,
+                                        nesterov=conf.optimizer.nesterov)
+        loss_ce = nn.CrossEntropyLoss()
+        train_loader = self.train_data.loader(local_batch_size, self.cid, shuffle=True, seed=conf.seed)
+        local_epoch_loss = []
+        for epoch in range(local_epoch):
+            batch_loss = []
+            for data, label in train_loader:
+                data, label = data.to(device), label.to(device)
+                data, label = Variable(data), Variable(label)
+                out = self.local_model(data)
+                local_optimizer.zero_grad()
+                loss = loss_ce(out, label)
+
+                loss.backward()
+                local_optimizer.step()
+                batch_loss.append(loss.item())
+            local_epoch_loss.append(sum(batch_loss) / len(batch_loss))
+        local_loss = sum(local_epoch_loss) / len(local_epoch_loss)
+        print("--- local_update_loss : {:.2f}".format(local_loss))
+        self.test_local_model(conf, device)
+
+    def train_DML(self, conf, device):
+        model_A = copy.deepcopy(self.model)
+        model_B = copy.deepcopy(self.local_model)
+        model_A.to(device)
+        model_B.to(device)
+        optimizer_A = torch.optim.SGD(model_A.parameters(),
+                                        lr=DML_lr,
+                                        momentum=conf.optimizer.momentum,
+                                        weight_decay=conf.optimizer.weight_decay,
+                                        nesterov=conf.optimizer.nesterov)
+        optimizer_B = torch.optim.SGD(model_B.parameters(),
+                                        lr=DML_lr,
+                                        momentum=conf.optimizer.momentum,
+                                        weight_decay=conf.optimizer.weight_decay,
+                                        nesterov=conf.optimizer.nesterov)
+        loss_kl = nn.KLDivLoss(reduction='batchmean')
+        loss_ce = nn.CrossEntropyLoss()
+        train_loader = self.train_data.loader(DML_batch_size, self.cid, shuffle=True, seed=conf.seed)
+        
+        
+        A_epoch_loss = []
+        for epoch in range(DML_epoch):
+            A_batch_loss = []
+            for data, label in train_loader:
+                data, label = data.to(device), label.to(device)
+                data, label = Variable(data), Variable(label)
+
+                out_A = model_A(data)
+                out_B = model_B(data)
+
+                optimizer_A.zero_grad()
+                optimizer_B.zero_grad()
+
+                loss_A = beta * loss_ce(out_A, label) + alpha * loss_kl(F.log_softmax(out_A, dim=1), F.softmax(out_B, dim=1))
+                loss_B = beta * loss_ce(out_B, label) + alpha * loss_kl(F.log_softmax(out_B, dim=1), F.softmax(out_A, dim=1))
+
+                loss_A.backward(retain_graph=True)
+                loss_B.backward(retain_graph=True)
+
+                optimizer_A.step()
+                optimizer_B.step()
+
+                A_batch_loss.append(loss_A.item())
+            A_epoch_loss.append(sum(A_batch_loss) / len(A_batch_loss))
+        A_loss = sum(A_epoch_loss) / len(A_epoch_loss)
+        print("--- DML_update_loss(A model) with Client:{}: {:.2f}".format(self.cid, A_loss))
+        # self.local_model = copy.deepcopy(self.model)
+        # self.local_model = copy.deepcopy(model_A)
+        self.local_model.load_state_dict(model_A.state_dict())
+        self.test_local_model(conf, device)
+    
+    def test_local_model(self, conf, device):
+        self.local_model.eval()
+        self.local_model = self.local_model.to(device)
+        test_loader = self.test_data.loader(100, self.cid, shuffle=False, seed=conf.seed)
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, label in test_loader:
+                data, label = data.to(device), label.to(device)
+                data, label = Variable(data), Variable(label)
+                out = self.local_model(data)
+                test_loss += F.cross_entropy(out, label, reduction='sum').item()
+                pred = out.max(1, keepdim=True)[1]
+                correct += pred.eq(label.view_as(pred)).sum().item()
+        test_loss /= len(test_loader.dataset)
+        test_acc = 100. * correct / len(test_loader.dataset)
+        # print("--- local_test_loss : {:.2f}".format( test_loss))
+        # print("--- local_test_acc : {:.2f}%".format( test_acc))
+        return test_loss, test_acc
+
+    def train(self, conf, device, train_local_only):
+        """
+        每个round中
+        1. A组客户端调用一次train，train_local_only为True，单独训练本地模型
+        2. B组客户端调用一次train，train_local_only为False，与server分发过来的train过的A组模型进行DML
+        """
+        if train_local_only:
+            self.train_local(conf, device)
+        else:
+            self.train_DML(conf, device)
+
+    def run_train(self, model, conf, train_local_only):
+        self.conf = conf
+        if conf.track:
+            self._tracker.set_client_context(conf.task_id, conf.round_id, self.cid)
+        self._is_train = True
+        self.download(model) 
+        self.track(metric.TRAIN_DOWNLOAD_SIZE, model_size(model))
+        self.decompression()
+
+        self.pre_train()
+        self.train(conf, self.device, train_local_only) # 只修改了这里
+        self.post_train()
+
+        self.track(metric.TRAIN_ACCURACY, self.train_accuracy)
+        self.track(metric.TRAIN_LOSS, self.train_loss)
+        self.track(metric.TRAIN_TIME, self.train_time)
+        
+        if conf.local_test:
+            self.test_local()
+        self.compression()
+        self.track(metric.TRAIN_UPLOAD_SIZE, model_size(self.compressed_model))
+        self.encryption()
+
+        return self.upload()
