7c04ac105f97db78af2fcdf3f0bd972e164108de
StreamAzure <1905105036@qq.com>
2023-04-20 10:32:03 +0000

    用CIFAR100会报驱动层面的错，放弃

--- main/femnist/baseline.py
+++
@@ -1,9 +0,0 @@
-import easyfl
-
-# Initialize federated learning with default configurations.
-config_file = "config.yaml"
-# Load and combine these two configs.
-config = easyfl.load_config(config_file)
-easyfl.init(config)
-# Execute federated learning training.
-easyfl.run()
