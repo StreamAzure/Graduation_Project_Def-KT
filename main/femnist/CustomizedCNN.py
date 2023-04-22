6e639edbf8e82515cf81d7bdbe8c82a3827fa06f
StreamAzure <1905105036@qq.com>
2023-04-22 07:40:12 +0000

    femnist baseline2000rounds

---
+++ main/femnist/CustomizedCNN.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class CustomizedCNN(nn.Module):
+    def __init__(self):
+        super(CustomizedCNN, self).__init__()
+        #输入为MNIST，张量(1,28,28)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        #经过卷积层后，张量(32,26,26)
+        #再经过2*2的最大池化层，张量(32,13,13)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        #经过卷积层后，张量(64,11,11)
+        #再经过2*2的最大池化层，张量(64,5,5)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
+        #经过卷积层后，张量(64,3,3)
+        #64*3*3=576
+        self.fc1 = nn.Linear(576, 64)
+        self.fc2 = nn.Linear(64, 62)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = self.conv3(x)
+        # 展平张量
+        x = x.view(-1, 576) # 576 = 64 * 3 * 3
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+        
--- main/femnist/baseline.py
+++ main/femnist/baseline.py
@@ -2,35 +2,10 @@ from torch import nn
 import torch.nn.functional as F
 import easyfl
 from easyfl.models import BaseModel
-
-# Define a customized model class.
-class CustomizedModel(BaseModel):
-    def __init__(self, channels=32):
-        super(CustomizedModel, self).__init__()
-        self.num_channels = channels
-        self.conv1 = nn.Conv2d(1, self.num_channels, 3, stride=1)
-        self.conv2 = nn.Conv2d(self.num_channels, self.num_channels * 2, 3, stride=1)
-        self.conv3 = nn.Conv2d(self.num_channels * 2, self.num_channels * 2, 3, stride=1)
-
-        self.fc1 = nn.Linear(4 * 4 * self.num_channels * 2, self.num_channels * 2)
-        self.fc2 = nn.Linear(self.num_channels * 2, 10)
-
-    def forward(self, s):
-        s = F.pad(s, (2, 2, 2, 2)) # 在图像边缘进行填充
-        s = self.conv1(s)  # batch_size x num_channels x 32 x 32
-        s = F.relu(F.max_pool2d(s, 2))  # batch_size x num_channels x 16 x 16
-        s = self.conv2(s)  # batch_size x num_channels*2 x 16 x 16
-        s = F.relu(F.max_pool2d(s, 2))  # batch_size x num_channels*2 x 8 x 8
-        s = self.conv3(s)  # batch_size x num_channels*2 x 8 x 8
-        # flatten the output for each image
-        s = s.view(-1, 4 * 4 * self.num_channels * 2)  # batch_size x 4*4*num_channels*4
-        # apply 2 fully connected layers with dropout
-        s = F.relu(self.fc1(s))
-        s = self.fc2(s)  # batch_size x 10
-        return s
+from CustomizedCNN import CustomizedCNN
 
 # Register the customized model class.
-easyfl.register_model(CustomizedModel)
+easyfl.register_model(CustomizedCNN)
 
 config_file = "config.yaml"
 config = easyfl.load_config(config_file)
